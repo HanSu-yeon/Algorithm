3b815f11ee6d832c8f8d5bea77c6e52c1d00e6d4
Han-su512 <js7122420@gmail.com>
2021-10-20 23:46:30 +0900

    211020

--- js100/211019.js
+++ js100/211019.js
@@ -1,42 +1,42 @@
-// //문제41 소수인지 판별
+//문제41 소수인지 판별
 
-// //내가 푼 방식
-// const num = 6;
+//내가 푼 방식
+const num = 6;
 
-// if (num % 2 !== 0) {
-//   console.log("소수 NO");
-// } else {
-//   console.log("소수 YES");
-// }
+if (num % 2 !== 0) {
+  console.log("소수 NO");
+} else {
+  console.log("소수 YES");
+}
 
-// //다른 사람 풀이
-// function check_prime(num) {
-//   for (let i = 2; i < num; i++) {
-//     const result = num % i;
-//     if (result === 0) {
-//       console.log("NO");
-//       return false;
-//     }
-//   }
-//   //1은 소수가 아니다
-//   if (num === 1) {
-//     console.log("NO");
-//     return;
-//   }
-//   console.log("YES");
-// }
+//다른 사람 풀이
+function check_prime(num) {
+  for (let i = 2; i < num; i++) {
+    const result = num % i;
+    if (result === 0) {
+      console.log("NO");
+      return false;
+    }
+  }
+  //1은 소수가 아니다
+  if (num === 1) {
+    console.log("NO");
+    return;
+  }
+  console.log("YES");
+}
 
-// check_prime(num);
+check_prime(num);
 
 // //문제42
-// function solution(a, b) {
-//   const week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-//   // new Date(년, 월, 일) 1월~12월은 0~11이고 일~토는 0~6
-//   const day = new Date(2020, a - 1, b);
-//   // const day = new Date("2020-" + a + "-" + b); 또는 이렇게 적을 수도 있다
-//   return week[day.getDay()];
-// }
-// solution(5, 24);
+function solution(a, b) {
+  const week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  // new Date(년, 월, 일) 1월~12월은 0~11이고 일~토는 0~6
+  const day = new Date(2020, a - 1, b);
+  // const day = new Date("2020-" + a + "-" + b); 또는 이렇게 적을 수도 있다
+  return week[day.getDay()];
+}
+solution(5, 24);
 
 //문제43 10진수를 2진수로 변환
 //내가 푼 방식
