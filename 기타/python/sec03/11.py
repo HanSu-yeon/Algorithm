565bdb895bfc0204844bcb97fbb6f15effb13412
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-05-28 00:06:20 +0900

    설명 추가

--- 기타/python/sec03/11.py
+++ 기타/python/sec03/11.py
@@ -9,7 +9,9 @@ for i in range(3):
     tmp=board[j][i:i+5]
     if tmp==tmp[::-1]:
       cnt+=1
-    for k in range(2):
+    #세로줄은 리스트가 아니니까 slice못쓴다
+    for k in range(2): 
+      #5//2의 결과인 2만큼만 돈다 -->ex)0열일때 board[0][0]와 board[4][0]비교, board[1][0]와 board[3][0]비교한다
       if board[i+k][j]!=board[i+5-k-1][j]:
         break
     else:
