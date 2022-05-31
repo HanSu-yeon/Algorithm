6767f093fc80d6520fc2f000e3be16ac42e3f931
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-05-31 23:11:00 +0900

    주석추가

--- 기타/python/sec04/02.py
+++ 기타/python/sec04/02.py
@@ -1,26 +1,30 @@
 import sys
-sys.stdin=open("input.txt", "r")
-def Count(len):
-    cnt=0
-    for x in Line:
-        cnt+=(x//len)
-    return cnt
+sys.stdin=open("input.txt","r")
 
-k, n=map(int, input().split())
+#len이 답이 될 수 있는지 확인하는 과정
+def Count(len):
+  cnt=0
+  for x in Line:
+    cnt+=(x//len)
+  return cnt
+
+k, n=map(int,input().split())
 Line=[]
 res=0
-largest=0
+largest=0 #가장 긴 랜선
+
 for i in range(k):
-    tmp=int(input())
-    Line.append(tmp)
-    largest=max(largest, tmp)
-lt=1
-rt=largest
-while lt<=rt:
-    mid=(lt+rt)//2
-    if Count(mid)>=n:
-        res=mid
-        lt=mid+1
-    else:
-        rt=mid-1
+  tmp = int(input())
+  Line.append(tmp)
+  largest = max(largest, tmp)
+
+start=1
+end=largest
+while start <= end:
+  mid = (start+end)//2  #mid=랜선의 길이
+  if Count(mid)>=n:
+    res=mid
+    start=mid+1
+  else:
+    end=mid-1
 print(res)
