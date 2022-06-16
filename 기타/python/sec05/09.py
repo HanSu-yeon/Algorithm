067b4d8d6e5123156f73ab0af91cdb3b16865bf4
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-06-16 09:08:28 +0900

    아나그램 개선코드

--- 기타/python/sec05/09.py
+++ 기타/python/sec05/09.py
@@ -4,22 +4,17 @@ sys.stdin=open("input.txt","rt")
 a=input()
 b=input()
 
-str1 = dict()
-str2 = dict()
-
+strHash = dict()
 for x in a:
-  str1[x]=str1.get(x, 0)+1
+  strHash[x]=strHash.get(x, 0)+1
 
 for y in b:
-  str2[y]=str2.get(y, 0)+1 
+  strHash[y]=strHash.get(y, 0)-1
 
-for i in str1.keys():
-  if i in str2.keys():
-    if str1[i] != str2[i]: #key의 개수 비교
-      print("NO")
-      break
-  else: #키가 한쪽에만 있다면
+for key in a:
+  if strHash.get(key)>0:
     print("NO")
     break
 else:
-  print("YES")
+  print("YES") 
+
