9b536170de71def92f68c5187ff925fd7b67e604
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-07-13 10:10:54 +0900

    암호제작-풀이수정

--- boj/1837.py
+++ boj/1837.py
@@ -1,17 +1,11 @@
 import sys
 sys.stdin=open("input.txt","r")
 
-
-def check(p, k):
-  for i in range(2, k):
-    if P % i == 0:
-      print("BAD ", i)
-      return
-  print("GOOD")
-  return
-
 P, K =map(int, input().split())
-check(P, K)
 
-
-
+for i in range(2, K):
+  if P % i == 0:
+    print("BAD", i)
+    break
+else:
+  print("GOOD")
