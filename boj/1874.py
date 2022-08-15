b4ce7bee2ba2507396bd111807725ca89e30afb5
HanSuYeon <js7122420@gmail.com>
2022-08-16 01:33:38 +0900

    스택 수열

--- boj/1874.py
+++ boj/1874.py
@@ -1,21 +1,28 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
+input = sys.stdin.readline
 
-n = int(input())
-stack = []
-res = []
-count = 1  # 현재까지 넣은 수라고 생각하면 될 거 같다
-for i in range(1, n+1):
-    data = int(input())
+n = int(input())  # 수의 개수
+stack = []  # stack 초기화
+result = []
+current = 1  # 현재 삽입할 수
 
-    while count <= data:
-        stack.append(count)
-        res.append('+')
-        count += 1
-    if stack[-1] == data:
-        a = stack.pop()
-        res.append('-')
+for _ in range(n):
+    x = int(input())
+    # top()보다 x가 더 크다면, 스택에 삽입
+    while len(stack) == 0 or stack[-1] < x:
+        stack.append(current)
+        current += 1
+        result.append("+")
+    # top()과 x가 같다면 스택에서 제거
+    if stack[-1] == x:
+        stack.pop()
+        result.append("-")
+    # top()보다 x가 더 작다면, 불가능
     else:
-        print('NO')
-        exit(0)
+        result = ["NO"]
+        break
+
+for x in result:
+    print(x)
