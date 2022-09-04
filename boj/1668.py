74b18394e8c918906b18c202e2c6541bd81c7981
HanSuYeon <js7122420@gmail.com>
2022-09-04 20:47:14 +0900

    풀이 추가

--- boj/1668.py
+++ boj/1668.py
@@ -5,12 +5,11 @@ n = int(input())
 arr = []
 for _ in range(n):  # 입력값 배열에 넣기
     arr.append(int(input()))
-print(arr)
-right_arr = []
-left_arr = []
+right_arr = []  # 오른쪽
+left_arr = []  # 왼쪽
 
 for x in arr:
-    if len(right_arr) == 0:
+    if len(right_arr) == 0:  # 배열이 비었으면
         right_arr.append(x)
     elif x > max(right_arr):
         right_arr.append(x)
@@ -24,3 +23,26 @@ for y in reversed(arr):
 
 print(len(right_arr))
 print(len(left_arr))
+
+# ---다른사람 풀이---
+
+
+def ascending(arr):
+    now = arr[0]
+    result = 1
+    for i in range(1, len(arr)):
+        if now < arr[i]:
+            result += 1
+            now = arr[i]
+    return result
+
+
+n = int(input())
+arr = []
+
+for _ in range(n):
+    arr.append(int(input()))
+
+print(ascending(arr))
+arr.reverse()
+print(ascending(arr))
