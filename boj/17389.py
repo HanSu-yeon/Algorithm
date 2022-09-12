799ffd6598d77e4a08a82387062fa9f92e87598f
HanSuYeon <js7122420@gmail.com>
2022-09-12 18:49:30 +0900

    풀이추가

--- boj/17389.py
+++ boj/17389.py
@@ -1,5 +1,10 @@
 import sys
 sys.stdin = open("input.txt", "r")
+'''
+N의 제한이 10,000
+시간복잡도: O(N)
+'''
+
 
 n = int(input())
 s = input()
@@ -11,6 +16,19 @@ for i in range(n):
         score += bonus
         bonus += 1
     elif s[i] == 'X':
+        bonus = 0
+
+print(score)
+
+# 다른 사람 풀이
+N, S = input(), input()
+
+score, bonus = 0, 0
+
+for idx, OX in enumerate(S):
+    if OX == 'O':
+        score, bonus = score+idx+1+bonus, bonus+1
+    else:
         bonus = 0
 
 print(score)
