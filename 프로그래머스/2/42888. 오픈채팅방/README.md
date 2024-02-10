7390662b41b005458ba5c8da39e278c5f070c007
HanSuyeon <71827641+HanSu-yeon@users.noreply.github.com>
2024-02-10 18:52:15 +0900

    [level 2] Title: 오픈채팅방, Time: 191.22 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/README.md
+++ 프로그래머스/2/42888. 오픈채팅방/README.md
@@ -1,10 +1,10 @@
 # [level 2] 오픈채팅방 - 42888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888) 
 
 ### 성능 요약
 
-메모리: 82.2 MB, 시간: 163.06 ms
+메모리: 81.6 MB, 시간: 191.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 6일 18:31:47
+2024년 2월 6일 18:52:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -2,19 +2,26 @@ function solution(records) {
   let answer = [];
   let userNickname = new Map();
   //유저와 닉네임 관리하는 map
-  for (let i = 0; i < records.length; i++) {
-    let [msg, id, nickname] = records[i].split(' ');
-    userNickname.set(id, nickname || userNickname.get(id));
-  }
+  // for (let i = 0; i < records.length; i++) {
+  //   let [msg, id, nickname] = records[i].split(' ');
+  //   userNickname.set(id, nickname || userNickname.get(id));
+  // }
+    
+    records.forEach(record =>{
+        const [msg, id, nickname] = record.split(' ');
+        if(msg ==='Enter' ||msg ==='Change'){
+            userNickname.set(id, nickname);
+        }
+    })
 
-  for (let i = 0; i < records.length; i++) {
-    let [msg, id] = records[i].split(' ');
-    if (msg == 'Enter') {
-      answer.push(`${userNickname.get(id)}님이 들어왔습니다.`);
+    records.forEach(record =>{
+      let [msg, id] = record.split(' ');
+      if (msg == 'Enter') {
+       answer.push(`${userNickname.get(id)}님이 들어왔습니다.`);
     } else if (msg == 'Leave') {
-      answer.push(`${userNickname.get(id)}님이 나갔습니다.`);
+       answer.push(`${userNickname.get(id)}님이 나갔습니다.`);
     }
-  }
+ });
 
   return answer;
 }
